72d3e2cf53c73d1d493da5f13d719996768bc928
Pragya-19 <138296470+Pragya-19@users.noreply.github.com>
2026-04-26 09:43:00 +0530

    Delete tests/tests/test_text_preprocessing.py

--- tests/tests/test_text_preprocessing.py
+++
@@ -1,8 +0,0 @@
-from nlp_model.text_preprocessor import clean_text
-
-
-def test_text_cleaning():
-    text = "Hello!!! How are you??"
-    result = clean_text(text)
-
-    assert result == "hello how are you"
